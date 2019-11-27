b4d4ada1fd880d8464e20419f8652b0cb3c983c4
nkarpushkin <karpushkin.na@gmail.com>
2019-11-27 21:33:13 +0900

    Pattern'ы по отдельности понятны, сделать адекватную комбинацию пока что не выходит, необходимо это исправить.

---
+++ Lesson_4/Homework/MainReg.java
@@ -0,0 +1,26 @@
+package Lesson_4.Homework;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainReg {
+    public static void main(String[] args) {
+        System.out.println(find("Java"));
+        System.out.println(find("1Gava"));
+        System.out.println(find("Gava"));
+        System.out.println(find("ava"));
+        System.out.println(find("Javadsadff"));
+        System.out.println(find("Java8password"));
+        System.out.println(find("123asd789Fgh1 31415"));
+        System.out.println(find("123asd789Fgh13141513423Jk"));
+
+    }
+
+    public static boolean find (String str) {
+//        Pattern pattern = Pattern.compile(".*\\d.*${4,20}");
+        Pattern pattern = Pattern.compile("[.\\d.\\S.*]{4,20}$");
+        Matcher matcher = pattern.matcher(str);
+
+        return matcher.matches();
+    }
+}
