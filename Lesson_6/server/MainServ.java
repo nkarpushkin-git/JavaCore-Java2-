0e62d98aa472c25001b86e1a152af1b16cbb9c17
nkarpushkin <karpushkin.na@gmail.com>
2019-12-04 23:08:57 +0900

    Домашняя работа. Урок 6. Не смог запустить клиента. Странно... Сделанное на прошлом уроке приложение клиент запускается

---
+++ Lesson_6/server/MainServ.java
@@ -0,0 +1,56 @@
+package Lesson_6.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class MainServ {
+    private Vector<ClientHandler> clients;
+
+    public MainServ() {
+        clients = new Vector<>();
+        ServerSocket server = null;
+        Socket socket = null;
+        try {
+            server = new ServerSocket(8189);
+            System.out.println("Сервер запущен!");
+
+            while (true) {
+                socket = server.accept();
+                System.out.println("Клиент подключился!");
+                clients.add(new ClientHandler(this, socket));
+                Scanner scanner = new Scanner(socket.getInputStream());
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void broadcastMsg(String msg) {
+        for (ClientHandler o: clients) {
+            o.sendMsg(msg);
+        }
+    }
+
+    // удаляем клиента из коллекции при выходе из чата
+    public void removeClient(ClientHandler client) {
+        clients.remove(client);
+    }
+}
